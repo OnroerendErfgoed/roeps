ed8da86424719c6cde3d040c78eda43030807524
degrookp <korneel.degroote@rwo.vlaanderen.be>
2016-10-13 16:21:21 +0200

    update roeps

--- R/roeps.R
+++ R/roeps.R
@@ -3,50 +3,66 @@ library(R6)
 
 OepsClient <- R6Class("OepsClient",
                       public = list(
-                          endpoint = NA,
-                          initialize = function(endpoint) {
-                              if (!missing(endpoint)) {
-                                  self$endpoint <- endpoint
-                              }
-                              else {
-                                  self$endpoint <- "https://inventaris.onroerenderfgoed.be/erfgoed/node"
-                              }
-                          },
-                          get_query_results = function (q_url) {
-                              url = paste(self$endpoint, q_url, sep='?')
-                              erfgoedobjecten_df <- private$combine_paginated_results(url)
-                              return(erfgoedobjecten_df)
-                          },
-                          get_erfgoedobject = function (e_url) {
-                              e_obj <- fromJSON(e_url)
-                              return(e_obj)
+                        nodetype = NA,
+                        endpoint = NA,
+                        initialize = function(nodetype="erfgoedobject") {
+                          self$nodetype <- nodetype
+                          if (nodetype == 'erfgoedobject') {
+                            self$endpoint <- "https://inventaris.onroerenderfgoed.be/erfgoedobjecten"
                           }
+                          else {
+                            self$endpoint <- "https://inventaris.onroerenderfgoed.be/aanduidingsobjecten"
+                          }
+                        },
+                        get_query_results = function (q_url) {
+                          url = paste(self$endpoint, q_url, sep='?')
+                          print(url)
+                          objecten_df <- private$combine_paginated_results(url)
+                          return(objecten_df)
+                        },
+                        
+                        get_object = function (e_url) {
+                          e_obj <- fromJSON(e_url)
+                          return(e_obj)
+                        }
+                        
                       ),
-
+                      
                       private = list(
-                          as_data_frame = function (erfgoedobjecten) {
-                              id <- sapply(erfgoedobjecten, function (x) { x$id })
-                              type <- sapply(erfgoedobjecten, function (x) { x$type$label })
-                              vastgesteld <- sapply(erfgoedobjecten, function (x) { x$vastgesteld })
-                              omschrijving <- sapply(erfgoedobjecten, function (x) { x$omschrijving })
-                              provincie <- sapply(erfgoedobjecten, function (x) { x$locatie$provincie })
-                              gemeente <- sapply(erfgoedobjecten, function (x) { x$locatie$gemeente })
-                              r <- data.frame(id, type, vastgesteld, omschrijving, provincie, gemeente)
-                              colnames(r) <- c('id', 'type', 'vastgesteld', 'omschrijving', 'provincie', 'gemeente')
-                              return(r)
-                          },
-                          combine_paginated_results = function (url, df=NULL) {
-                              r <- fromJSON(url)
-                              hrefs <- sapply(r$items$links, function (x) x$href[2])
-                              erfgoedobjecten <- lapply(hrefs, function (x) {self$get_erfgoedobject(x)})
-                              erfgoedobjecten_df <- private$as_data_frame(erfgoedobjecten)
-                              if (!is.null(df)) {
-                                  erfgoedobjecten_df <- rbind(erfgoedobjecten_df, df)
-                              }
-                              if ("next" %in% names(r$links)) {
-                                  erfgoedobjecten_df <- private$combine_paginated_results(r$links$`next`$href, erfgoedobjecten_df)
-                              }
-                              return(erfgoedobjecten_df)
+                        as_data_frame = function (erfgoedobjecten) {
+                          if (self$nodetype == 'erfgoedobject') {
+                            id <- sapply(erfgoedobjecten, function (x) { x$id })
+                            type <- sapply(erfgoedobjecten, function (x) { x$type$label })
+                            omschrijving <- sapply(erfgoedobjecten, function (x) { x$omschrijving })
+                            provincie <- sapply(erfgoedobjecten, function (x) { x$locatie$provincie })
+                            gemeente <- sapply(erfgoedobjecten, function (x) { x$locatie$gemeente })
+                            r <- data.frame(id, type, omschrijving, provincie, gemeente)
+                            colnames(r) <- c('id', 'type', 'omschrijving', 'provincie', 'gemeente')
+                          } else {
+                            id <- sapply(erfgoedobjecten, function (x) { x$id })
+                            type <- sapply(erfgoedobjecten, function (x) { x$type$label })
+                            omschrijving <- sapply(erfgoedobjecten, function (x) { x$omschrijving })
+                            provincie <- sapply(erfgoedobjecten, function (x) { x$locatie$provincie })
+                            gemeente <- sapply(erfgoedobjecten, function (x) { x$locatie$gemeente })
+                            r <- data.frame(id, type, omschrijving, provincie, gemeente)
+                            colnames(r) <- c('id', 'type', 'omschrijving', 'provincie', 'gemeente')
                           }
+                          return(r)
+                        },
+                     
+                        combine_paginated_results = function (url, df=NULL) {
+                          r <- fromJSON(url)
+                          hrefs <- sapply(r$items$links, function (x) x$href[2])
+                          objecten <- lapply(hrefs, function (x) {self$get_object(x)})
+                          objecten_df <- private$as_data_frame(objecten)
+                          if (!is.null(df)) {
+                            objecten_df <- rbind(objecten_df, df)
+                          }
+                          if ("next" %in% names(r$links)) {
+                            next_link <- gsub('erfgoed/node/.json', paste(self$nodetype, 'en', sep=''), r$links$`next`$href)
+                            objecten_df <- private$combine_paginated_results(next_link, objecten_df)
+                          }
+                          return(objecten_df)
+                        }
                       )
 )
